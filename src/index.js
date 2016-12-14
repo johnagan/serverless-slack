7ce3c841da89ed45c2cc7f04b07a6739e4f8ac94
John Agan <jagan@slack-corp.com>
2016-12-14 08:31:30 -0800

    removed regex support... for now

--- src/index.js
+++ src/index.js
@@ -123,12 +123,6 @@ class Slack extends EventEmitter {
     // notify message button triggered by callback_id
     if (payload.callback_id) events.push('interactive_message', payload.callback_id);
 
-    // regex matches
-    let json = payload.toString();
-    this.eventNames().forEach(exp => {
-      if (exp instanceof RegExp && exp.test(json)) events.push(exp);
-    });
-
     // trigger all events
     events.forEach(name => this.emit(name, payload, bot, this.store));
   }
